1636d93ca450df3a9380baef7b5933744420f09c
Senjaya <amarpanjis@gmail.com>
2022-06-24 07:20:36 +0700

    add kafka consumer init (#2)
    * add kafka producer
    
    * change return type to reference
    
    * move kafka to queue package
    
    * fix package name
    
    * delete source and password on config struct
    
    * add kafka consumer init

--- queue/kafka.go
+++ queue/kafka.go
@@ -7,7 +7,7 @@ import (
 	"github.com/Shopify/sarama"
 )
 
-func InitKafka(brokerList []string) *sarama.AsyncProducer {
+func InitKafkaProducer(brokerList []string) *sarama.AsyncProducer {
 	var config = sarama.NewConfig()
 	config.Producer.RequiredAcks = sarama.WaitForLocal
 	config.Producer.Partitioner = sarama.NewRandomPartitioner
@@ -25,4 +25,14 @@ func InitKafka(brokerList []string) *sarama.AsyncProducer {
 	}()
 
 	return &producer
+}
+
+func InitKafkaConsumer(brokerList []string) *sarama.Consumer {
+	var config = sarama.NewConfig()
+	var consumer, err = sarama.NewConsumer(brokerList, config)
+	if err != nil {
+		panic(err)
+	}
+
+	return &consumer
 }
--- queue/queue.go
+++ queue/queue.go
@@ -5,12 +5,20 @@ import "context"
 type Config struct {
 	Driver string
 	Host   string
+	Kind   string
 }
 
 func Init(ctx context.Context, config Config) interface{} {
 	switch config.Driver {
 	case "kafka":
-		return InitKafka([]string{config.Host})
+		switch config.Kind {
+		case "producer":
+			return InitKafkaProducer([]string{config.Host})
+		case "consumer":
+			return InitKafkaConsumer([]string{config.Host})
+		default:
+			return nil
+		}
 	default:
 		return nil
 	}
