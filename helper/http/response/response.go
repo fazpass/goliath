2b548c8e9f07d1f7970e68892dc2cfb82ec709f5
Irvan Maulana <me@tukangremot.com>
2022-06-27 18:40:49 +0700

    feat(helper): add meta in http response

--- helper/http/response/response.go
+++ helper/http/response/response.go
@@ -3,11 +3,12 @@ package response
 import "net/http"
 
 type Response struct {
-	Status  bool        `json:"status"`
+	Status  bool        `json:"status,omitempty"`
 	Errors  interface{} `json:"errors,omitempty"`
-	Message string      `json:"message"`
-	Code    string      `json:"code"`
+	Message string      `json:"message,omitempty"`
+	Code    string      `json:"code,omitempty"`
 	Data    interface{} `json:"data,omitempty"`
+	Meta    interface{} `json:"meta,omitempty"`
 }
 
 func (response *Response) Render(w http.ResponseWriter, r *http.Request) error {
