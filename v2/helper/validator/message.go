69c42153930460318f670823b011578e3d0cf6b1
Irvan Maulana <me@tukangremot.com>
2022-07-18 09:23:43 +0700

    Update message.go

--- v2/helper/validator/message.go
+++ v2/helper/validator/message.go
@@ -8,6 +8,10 @@ func GetMessage(field string, tag string) string {
 		return fmt.Sprint("The ", field, " field is required")
 	case "email":
 		return fmt.Sprint("The ", field, " field must be a valid email address.")
+	case "numeric":
+		return fmt.Sprint("The ", field, " field must be a valid numeric.")
+	case "uuid":
+		return fmt.Sprint("The ", field, " field must be a valid uuid.")
 	default:
 		return tag
 	}
