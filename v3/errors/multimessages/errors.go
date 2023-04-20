98676f6a426a9a6382dea8ff9bb5f02b0aa787af
Irvan Maulana <me@tukangremot.com>
2023-04-20 22:54:27 +0700

    fix(v3/errors): handle get with empty key (#9)
    * feat(v3/errors): add multi value messages
    
    * fix(v3/errors): handle get with empty key

--- v3/errors/multimessages/errors.go
+++ v3/errors/multimessages/errors.go
@@ -30,9 +30,16 @@ func New(httpStatus int, caseCode string, messages map[string]string) *Error {
 }
 
 func (e *Error) Get(key string) error {
+
 	message, ok := e.Errors[key]
 	if ok {
 		return message
+	}
+
+	if key == "" {
+		for _, err := range e.Errors {
+			return err
+		}
 	}
 
 	return goliatherrors.New(e.HttpStatus, e.CaseCode, DefaultMessage)
